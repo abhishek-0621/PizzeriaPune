3613e562948b42748a0359210d0c0157a80ff1b6
Rutuja Khire <rutujask99@gmail.com>
2021-03-30 00:25:04 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$database = "foodorderingsystem";
+	$database = "pizzadb";
 	
 	
 	$conn = mysqli_connect($servername, $username, $password,$database);
